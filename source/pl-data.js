db1e0813cfdd537f097231b20c5d84f9dfb451dd
lCaba1 <a.m.kabashov@gmail.com>
2024-12-14 15:44:11 +0300

    -

--- source/pl-data.js
+++ source/pl-data.js
@@ -3,78 +3,90 @@ export const menu = [
         keyword: 'gazpacho',
         name: 'Гаспачо',
         price: 195,
-        category: 'menu-soup',
+        category: 'soup',
         count: '350г',
         image: '../data/gazpacho.jpg'
-    },
-    {
-        keyword: 'mushroom soup',
-        name: 'Грибной суп-пюре',
-        price: 185,
-        category: 'menu-soup',
-        count: '330г',
-        image: '../data/mushroom_soup.jpg'
-    },
-    {
-        keyword: 'norwegian soup',
-        name: 'Норвежский суп',
-        price: 270,
-        category: 'menu-soup',
-        count: '330г',
-        image: '../data/norwegian_soup.jpg'
     },
     {
         keyword: 'fried potatoes with mushrooms',
         name: 'Жареная картошка с грибами',
         price: 150,
-        category: 'menu-main',
+        category: 'main',
         count: '250г',
         image: '../data/friedpotatoeswithmushrooms1.jpg'
-    },
-    {
-        keyword: 'lasagna',
-        name: 'Лазанья',
-        price: 385,
-        category: 'menu-main',
-        count: '310г',
-        image: '../data/lasagna.jpg'
-    },
-    {
-        keyword: 'chicken cutlets and mashed potatoes',
-        name: 'Котлеты из курицы с картофельным пюре',
-        price: 225,
-        category: 'menu-main',
-        count: '280г',
-        image: '../data/chickencutletsandmashedpotatoes.jpg'
-    },
-    {
-        keyword: 'orange juice',
-        name: 'Апельсиновый сок',
-        price: 120,
-        category: 'menu-drink',
-        count: '300мл',
-        image: '../data/orangejuice.jpg'
-    },
-    {
-        keyword: 'apple juice',
-        name: 'Яблочный сок',
-        price: 90,
-        category: 'menu-drink',
-        count: '300мл',
-        image: '../data/applejuice.jpg'
     },
     {
         keyword: 'carrot juice',
         name: 'Морковный сок',
         price: 110,
-        category: 'menu-drink',
+        category: 'drink',
         count: '300мл',
         image: '../data/carrotjuice.jpg'
+    },
+    {
+        keyword: 'mushroom soup',
+        name: 'Грибной суп-пюре',
+        price: 185,
+        category: 'soup',
+        count: '330г',
+        image: '../data/mushroom_soup.jpg'
+    },
+    {
+        keyword: 'lasagna',
+        name: 'Лазанья',
+        price: 385,
+        category: 'main',
+        count: '310г',
+        image: '../data/lasagna.jpg'
+    },
+    {
+        keyword: 'orange juice',
+        name: 'Апельсиновый сок',
+        price: 120,
+        category: 'drink',
+        count: '300мл',
+        image: '../data/orangejuice.jpg'
+    },
+    {
+        keyword: 'norwegian soup',
+        name: 'Норвежский суп',
+        price: 270,
+        category: 'soup',
+        count: '330г',
+        image: '../data/norwegian_soup.jpg'
+    },
+    {
+        keyword: 'chicken cutlets and mashed potatoes',
+        name: 'Котлеты из курицы с картофельным пюре',
+        price: 225,
+        category: 'main',
+        count: '280г',
+        image: '../data/chickencutletsandmashedpotatoes.jpg'
+    },
+    {
+        keyword: 'apple juice',
+        name: 'Яблочный сок',
+        price: 90,
+        category: 'drink',
+        count: '300мл',
+        image: '../data/applejuice.jpg'
     }
 ]
 
 export const headers = {
-    'menu-soup': 'Выберите суп',
-    'menu-main': 'Выберите главное блюдо',
-    'menu-drink': 'Выберите напиток'
+    'soup': 'Выберите суп',
+    'main': 'Выберите главное блюдо',
+    'drink': 'Выберите напиток'
+}
+
+export const labels = {
+    'soup': 'Суп',
+    'main': 'Главное блюдо',
+    'drink': 'Напиток'
+}
+
+export const defaults = {
+    'soup': 'Суп не выбран',
+    'main': 'Главное блюдо не выбрано',
+    'drink': 'Напиток не выбран'
 }
--- source/pl-show.js
+++ source/pl-show.js
@@ -1,55 +1,102 @@
-import { menu } from './pl-data.js'
-import { headers } from './pl-data.js';
+import {  menu, headers, labels, defaults } from './pl-data.js'
 
 const main = document.getElementById('main-container');
+const fieldset = document.getElementById('order-fieldset');
+const insertTo = fieldset.children[fieldset.children.length - 4]; // where to insert in fieldset
+
+let totalPrice = 0;
+
+let sectionId;      // "soup" "main" "drink"
+let section;        // "section" tag with "id" = "menu-" + sectionId
+let header;         // "h2" tag in section
+let gridContainer;  // "div" tag in section with "class" = "grid-container"
+let label;          // "label" tag in fieldset
+let br;             // "br" tag
+let select;         // "select" tag in fieldset
+let option;         // "option" tag in select
+let gridItem;       // "div" label in gridContainer with "class" = "grid-item"
 
 menu.forEach(dish => {
-    const sectionId = dish.category;
-    let currentSection = document.getElementById(sectionId);
+    sectionId = dish.category;
+    section = document.getElementById(`menu-${sectionId}`);
 
-    if (!currentSection) {
-        const newSection = document.createElement('section');
-        newSection.id = sectionId;
-        main.insertBefore(newSection, main.lastElementChild);
+    // first time add: new section with header/gridContainer to main, new label/select with option to fieldset
+    if (!section) {
+        section = document.createElement('section');
+        section.id = `menu-${sectionId}`;
+        main.insertBefore(section, main.lastElementChild);
 
-        const header = document.createElement('h2');
+        header = document.createElement('h2');
         header.textContent = headers[sectionId];
-        newSection.appendChild(header);
+        section.appendChild(header);
 
-        const newGridContainer = document.createElement('div');
-        newGridContainer.className = 'grid-container';
-        newSection.appendChild(newGridContainer);
+        gridContainer = document.createElement('div');
+        gridContainer.className = 'grid-container';
+        section.appendChild(gridContainer);
 
-        currentSection = newSection;
+        label = document.createElement('label');
+        label.textContent = labels[sectionId];
+        label.setAttribute('for', `order-${dish.category}`);
+        fieldset.insertBefore(label, insertTo);
+
+        br = document.createElement('br');
+        fieldset.insertBefore(br, insertTo);
+
+        select = document.createElement('select');
+        select.name = `order-${dish.category}`;
+        select.id = `order-${dish.category}`;
+        select.required = true;
+        fieldset.insertBefore(select, insertTo);
+
+        br = document.createElement('br');
+        fieldset.insertBefore(br, insertTo);
+
+        option = document.createElement('option');
+        option.value = '';
+        option.textContent = defaults[sectionId];
+        option.disabled = true;
+        option.selected = true;
+        option.hidden = true;
+        select.appendChild(option);
     }
 
-    const newGridItem = document.createElement('div');
-    newGridItem.className = 'grid-item';
-    newGridItem.setAttribute('data-dish', dish.keyword);
-    const currentGridContainer = currentSection.querySelector('div');
-    currentGridContainer.appendChild(newGridItem);
-
-    const currentGridItem = newGridItem;
+    gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+    gridItem.setAttribute('data-dish', dish.keyword);
+    gridContainer = section.querySelector('div');
+    gridContainer.appendChild(gridItem);
 
     const image = document.createElement('img');
     image.src = dish.image;
     image.alt = dish.keyword;
-    currentGridItem.appendChild(image);
+    gridItem.appendChild(image);
 
     const price = document.createElement('p');
     price.textContent = dish.price;
-    currentGridItem.appendChild(price);
+    gridItem.appendChild(price);
 
     const name = document.createElement('p');
     name.textContent = dish.name;
-    currentGridItem.appendChild(name);
+    gridItem.appendChild(name);
 
     const count = document.createElement('p');
     count.textContent = dish.count;
-    currentGridItem.appendChild(count);
+    gridItem.appendChild(count);
 
     const button = document.createElement('button');
     button.textContent = 'Добавить';
-    currentGridItem.appendChild(button);
+    gridItem.appendChild(button);
+
+    select = document.getElementById(`order-${sectionId}`);
+
+    const option1 = document.createElement('option');
+    option1.value = dish.keyword;
+    option1.textContent = dish.name + ' ' + dish.price + 'р';
+    select.appendChild(option1);
+
+    button.addEventListener('click', () => {
+        totalPrice += price;
+    });
 });
+
 
